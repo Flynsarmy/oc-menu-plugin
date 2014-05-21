fd220fd43c1f854711cc1d3d92cf54663943c261
Sam Georges <sam@daftspunk.com>
2014-05-21 21:14:19 +1000

    Create AJAX handler for the creation form

--- formwidgets/ItemList.php
+++ formwidgets/ItemList.php
@@ -1,7 +1,9 @@
 <?php namespace Flynsarmy\Menu\FormWidgets;
 
 use Backend\Classes\FormWidgetBase;
+use Flynsarmy\Menu\Models\MenuItem;
 use Flynsarmy\Menu\Classes\MenuManager;
+use Exception;
 
 /**
  * Rich Editor
@@ -52,6 +54,33 @@ class ItemList extends FormWidgetBase
     {
         $this->vars['item_types'] = MenuManager::instance()->listItemTypes();
         return $this->makePartial('type_selection');
+    }
+
+    public function onLoadCreateItem()
+    {
+        try {
+            $type = post('type_id');
+            if (!$type = MenuManager::instance()->resolveItemType($type))
+                throw new Exception('Invalid item type: '. post('type_id'));
+
+            $this->vars['type'] = $type;
+            $itemTypes = MenuManager::instance()->listItemTypes();
+            $this->vars['itemType'] = $itemTypes[$type];
+
+            /*
+             * Create a form widget to render the form
+             */
+            $config = $this->makeConfig('@/plugins/flynsarmy/menu/models/menuitem/fields.yaml');
+            $config->model = new MenuItem;
+            $config->context = 'create';
+            $this->vars['form'] = $this->makeWidget('Backend\Widgets\Form', $config);
+
+        }
+        catch (Exception $ex) {
+            $this->vars['fatalError'] = $ex->getMessage();
+        }
+
+        return $this->makePartial('create_item');
     }
 
 }
