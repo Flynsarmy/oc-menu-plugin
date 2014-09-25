8e7f794390c715c7e38e107c43914e0d41ed7e4a
Peter O <that0n3guy@users.noreply.github.com>
2014-09-25 12:52:44 -0500

    Support custom itemType-specific output

--- components/Menu.php
+++ components/Menu.php
@@ -50,7 +50,7 @@ class Menu extends ComponentBase
 		$menu = MenuModel::with('items')->where('id', '=', $this->property('menu_id', 0))->get();
 
         foreach($menu as $singlemenu) // there is only one of these... but eager loading seems to make me do this
-            $this->menuTree= $this->buildTree($singlemenu);
+            $this->menuTree= $this->buildTree($singlemenu, $this->controller);
     }
 
     /**
@@ -58,26 +58,33 @@ class Menu extends ComponentBase
      * The same thing could easily be done in the twig template using methods like getEagerChildren(), but this queries
      *      the database more.
      *
-     * @param $elements
+     * @param $items
      * @param int $parentId
+     * @param $controller
      * @return array
      */
-    function buildTree($elements, $parentId = 0) {
+    function buildTree($items, $controller, $parentId = 0) {
         $branch = array();
 
-        if(isset($elements->items)){
-            $items = $elements->items;
+        // check if this is the first run through of this function
+        if(isset($items->items)){
+            $items = $items->items;
         }
-        else {
-            $items = $elements;
-        }
-        foreach ($items as $element) {
-            if ($element->parent_id == $parentId) {
-                $children = $this->buildTree($items, $element->id);
+
+        foreach ($items as $item) {
+            if ($item->parent_id == $parentId) {
+                $children = $this->buildTree($items, $controller, $item->id);
                 if ($children) {
-                    $element->children = $children;
+                    $item->children = $children;
                 }
-                $branch[] = $element;
+                // Support custom itemType-specific output
+                if ( class_exists($item->master_object_class) )
+                {
+                    $itemTypeObj = new $item->master_object_class;
+                    if ( $render = $itemTypeObj->onRender($item, $controller) )
+                        $item->render = $render;
+                }
+                $branch[] = $item;
             }
         }
 
